10ec6642b7e8cc412203d166061687154c9cd43b
Valentin Edelsbrunner <v.edelsbrunner@protonmail.com>
2023-11-18 12:42:20 +0100

    Implements same position clustering

--- cluster/SamePositionClustering.py
+++ cluster/SamePositionClustering.py
@@ -4,4 +4,4 @@ from graph.GeoNetwork import GeoNetwork
 
 class SamePositionClustering(ClusteringStrategy):
     def cluster(self, network: GeoNetwork, **kwargs):
-        print("Clustering with same position")
+        network.gdf_points['cluster'] = network.gdf_points.groupby('geometry').ngroup()
