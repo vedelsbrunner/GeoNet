412bfd8bf1312de7c2fe74f91e3b566a107d37dc
Valentin Edelsbrunner <v.edelsbrunner@protonmail.com>
2023-11-20 18:25:42 +0100

    Manual place name ambiguities fixes for mariboucher dataset

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from cluster.DbscanClustering import DbscanClustering
 from cluster.SamePositionClustering import SamePositionClustering
 from graph.GeoNetwork import GeoNetwork
-from layouts.CircularLayout import CircularLayoutConfig
+from layouts.CircularLayoutConfig import CircularLayoutConfig
 from utils.Geocooder import geocode_places
 import shapely.wkt as wkt
 from input.ColumnNormalizer import marieboucher_mapping, normalize_column_names
@@ -16,6 +16,10 @@ from utils.LoggerConfig import logger
 def process_marieboucher_data():
     mariebouche = "./datasets/marieboucher.csv"
     df = pd.read_csv(mariebouche)
+    df.replace("Bouffay, Nantes", "Bouffay 44000 Nantes Frankreich", inplace=True)
+    df.replace("Ile Saint Christophe", "Saint-Christophe-et-Niévès", inplace=True)
+    df.replace(to_replace=r'\bSt\b', value='Saint', regex=True, inplace=True)
+
     df = normalize_column_names(df, marieboucher_mapping)
     df = geocode_places(df)
     df.to_csv("./datasets/marieboucher_geocoded.csv", index=False)
@@ -24,9 +28,10 @@ def process_marieboucher_data():
 def create_marie_boucher_geo_network():
     network = GeoNetwork()
     df = pd.read_csv("./datasets/marieboucher_geocoded.csv")
+
     for index, row in df.iterrows():
-        target_location = wkt.loads(row['target_location'])
-        source_location = wkt.loads(row['source_location'])
+        target_location = wkt.loads(row['target_coordinates'])
+        source_location = wkt.loads(row['source_coordinates'])
 
         if source_location.is_empty or target_location.is_empty:
             logger.info('Skipping empty location')
@@ -44,22 +49,37 @@ def create_marie_boucher_geo_network():
 
 
 def main():
-    # process_marieboucher_data()
-    marie_boucher_network = create_marie_boucher_geo_network()
-    # clustering_strategy = DbscanClustering(eps=0.1) #TODO: Cluster -1 ?
-    clustering_strategy = SamePositionClustering()
+    process_marieboucher_data()
+    network = create_marie_boucher_geo_network()
+    clustering_strategy = DbscanClustering(eps=0.5) #TODO: Cluster -1 ?
+    # network = GeoNetwork()
+    # network.create_fixed_clusters(seed=42)
+    # network.finalize()
+    # clustering_strategy = SamePositionClustering()
     layout_factory = LayoutFactory(clustering_strategy)
-    circular_layout_config = CircularLayoutConfig(radius_scale=0.4)
+    circular_layout_config = CircularLayoutConfig(radius_scale=5)
     circular_layout = layout_factory.get_layout(LayoutType.CIRCULAR)
-    circular_layout.create_layout(marie_boucher_network, circular_layout_config)
+    circular_layout.create_layout(network, circular_layout_config)
+    circular_layout.optimize_layout(network, max_iterations_per_cluster=100, improvement_threshold=1)
+    # for i in range(1, 1000):
+    #     logger.info(f"Total edge crossings: {crossings}")
+    #     crossings = network.calculate_total_edge_crossings()
+
+
+
+    # circular_layout.optimize_node_positions(network, circular_layout_config)
+    # crossings = network.calculate_total_edge_crossings()
+    # logger.info(f"Total edge crossings: {crossings}")
+
+    # circular_layout.apply_barycenter_heuristic(network=network, config=circular_layout_config)
 
     # stack_layout_config = StackedLayoutConfig(stack_points_offset=0.008, hull_buffer=0.01)
     # stacked_layout = layout_factory.get_layout(LayoutType.STACKED)
     # stacked_layout.create_layout(marie_boucher_network, stack_layout_config)
 
 
-    marie_boucher_network.write_to_disk('../thesis-demo/datas/marieboucher_points.geojson', '../thesis-demo/datas/marieboucher_lines.geojson')
-
+    network.write_to_disk('../thesis-demo/datas/marieboucher_points_bary.geojson', '../thesis-demo/datas/marieboucher_lines_bary.geojson')
+    # network.write_to_disk('../thesis-demo/datas/points_bary.geojson', '../thesis-demo/datas/lines_bary.geojson')
 
 if __name__ == '__main__':
     main()
