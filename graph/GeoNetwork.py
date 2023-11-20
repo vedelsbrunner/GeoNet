a914df9b07127128b76f57d6109156410324e617
Valentin Edelsbrunner <v.edelsbrunner@protonmail.com>
2023-11-20 11:38:49 +0100

    Uses multiprocessing for calculating the total number of edge crossings

--- graph/GeoNetwork.py
+++ graph/GeoNetwork.py
@@ -1,26 +1,31 @@
 import logging
-import time
+import random
+from itertools import combinations
+from multiprocessing import Pool
 
 import geopandas as gpd
 import networkx as nx
-import numpy as np
 import pandas as pd
 from pyproj import CRS
 from shapely.geometry import Point, LineString
-from sklearn.cluster import DBSCAN
 
 from utils.LoggerConfig import logger
 
 logging.basicConfig(level=logging.INFO)
 
+
+def check_crossings(pair):
+    line1, line2 = pair
+    return LineString(line1).crosses(LineString(line2))
+
+
 class GeoNetwork:
     def __init__(self):
         self.__gdf_points = gpd.GeoDataFrame(columns=['id', 'geometry'])
         self.__gdf_edges = gpd.GeoDataFrame(columns=['id', 'geometry'])
-        self.__points_data = [] # Only use for optimization purposes
+        self.__points_data = []  # Only use for optimization purposes
         self.__lines_data = []  # Only use for optimization purposes
         self.__point_to_edges = {}
-
         # // TODO: maybe private as well?
         self.graph = nx.Graph()
 
@@ -57,28 +62,23 @@ class GeoNetwork:
             self.__point_to_edges[point_id_start].append((line_id, point_id_end))
             self.__point_to_edges[point_id_end].append((line_id, point_id_start))
 
-    def finalize(self, crs="EPSG:32633"): #TODO: Ugly work around
+    # TODO: Ugly post-processing, refactor(!)
+    def finalize(self, crs="EPSG:32633"):
         points_df = pd.DataFrame(self.__points_data)
         lines_df = pd.DataFrame(self.__lines_data)
 
-        # Assert that the number of nodes and edges matches
         assert points_df['id'].nunique() == self.graph.number_of_nodes(), "Mismatch in number of points"
         assert lines_df['id'].nunique() == self.graph.number_of_edges(), "Mismatch in number of lines"
 
-        # Compute the degree for each node (point) in the NetworkX graph
         degrees = dict(self.graph.degree())
-        # Add a 'degree' column to the points DataFrame
         points_df['degree'] = points_df['id'].apply(lambda x: degrees.get(x, 0))
 
         closeness = nx.closeness_centrality(self.graph)
         betweenness = nx.betweenness_centrality(self.graph)
 
-        # Add 'closeness_centrality' and 'betweenness_centrality' columns to the points DataFrame
-        points_df['closeness_centrality'] = points_df['id'].apply(lambda x: closeness.get(x, 0))
-        points_df['betweenness_centrality'] = points_df['id'].apply(lambda x: betweenness.get(x, 0))
+        points_df['closeness_centrality'] = points_df['id'].apply(lambda x: closeness.get(x, 0)).round(2)
+        points_df['betweenness_centrality'] = points_df['id'].apply(lambda x: betweenness.get(x, 0)).round(2)
 
-
-        # Create the GeoNetwork as dataframes using the same crs
         crs_object = CRS(crs)
         self.__gdf_points = gpd.GeoDataFrame(points_df, geometry='geometry', crs=crs_object)
         self.__gdf_edges = gpd.GeoDataFrame(lines_df, geometry='geometry', crs=crs_object)
@@ -106,33 +106,93 @@ class GeoNetwork:
         new_line_geom = LineString([point_start_pos, point_end_pos])
         self.__gdf_edges.loc[self.__gdf_edges['id'] == line_id, 'geometry'] = new_line_geom
 
-
     def update_point_properties(self, point_id, **properties):
-        # Check if the point exists in the graph
         if point_id in self.graph:
             # Update the node properties in the graph
             for key, value in properties.items():
                 self.graph.nodes[point_id][key] = value
 
-            # Update the properties in the points GeoDataFrame
             for key, value in properties.items():
                 self.__gdf_points.loc[self.__gdf_points['id'] == point_id, key] = value
         else:
             logging.warning(f"Point with ID {point_id} not found in the graph.")
-
 
     def add_point_props(self, point_id, **props):
         if point_id not in self.graph:
             logging.warning(f"Point with ID {point_id} does not exist.")
             return
 
-        # Update properties in the networkx graph node
         for key, value in props.items():
             self.graph.nodes[point_id][key] = value
 
-        # Update properties in the GeoDataFrame
         if self.__gdf_points['id'].isin([point_id]).any():
             for key, value in props.items():
                 self.__gdf_points.loc[self.__gdf_points['id'] == point_id, key] = value
         else:
             logging.warning(f"Point with ID {point_id} not found in GeoDataFrame.")
+
+    def calculate_total_edge_crossings(self):
+        logger.debug("Calculating total edge crossings...")
+        edges = list(self.graph.edges())
+        lines = [(self.graph.nodes[edge[0]]['pos'], self.graph.nodes[edge[1]]['pos']) for edge in edges]
+        pairs = list(combinations(lines, 2))
+
+        with Pool() as pool:
+            results = pool.map(check_crossings, pairs)
+
+        total_crossings = sum(results)
+        return total_crossings
+
+
+    # Used for the CircularLayout TODO: Move to CircularLayout
+    def are_connections_internal(self, point_id):
+        point_cluster = self.__gdf_points.loc[self.__gdf_points['id'] == point_id, 'cluster'].values[0]
+        connected_points = [edge[1] for edge in self.__point_to_edges[point_id]]
+
+        for connected_point_id in connected_points:
+            connected_point_cluster = self.__gdf_points.loc[self.__gdf_points['id'] == connected_point_id, 'cluster'].values[0]
+            if connected_point_cluster != point_cluster:
+                return False
+        return True
+
+    # TODO: Remove - only used for testing
+    def create_fixed_clusters(self, seed=42):
+        random.seed(seed)
+
+        cluster_positions = {
+            'cluster_20': (2.3522, 48.8566),  # Coords for Paris, for example
+            'cluster_10': (3.8767, 43.6112),  # Coords for Montpellier
+            'cluster_5': (-1.6793, 48.1173),  # Coords for Rennes
+        }
+
+        cluster_sizes = {'cluster_20': 20, 'cluster_10': 10, 'cluster_5': 5}
+        for cluster_name, (lon, lat) in cluster_positions.items():
+            for i in range(cluster_sizes[cluster_name]):
+                point_id = f'{cluster_name}_point_{i}'
+                self.add_point(point_id, lon, lat)
+
+        all_points = list(self.graph.nodes)
+        num_lines = 50
+        for i in range(num_lines):
+            point_id_start, point_id_end = random.sample(all_points, 2)
+            line_id = f'line_{i}'
+            self.add_line(line_id, point_id_start, point_id_end)
+
+    def swap_nodes(self, node1_id, node2_id):
+        if node1_id not in self.graph or node2_id not in self.graph:
+            logger.warning(f"One or both nodes to swap do not exist: {node1_id}, {node2_id}")
+            return False
+
+        node1_pos = self.graph.nodes[node1_id]['pos']
+        node2_pos = self.graph.nodes[node2_id]['pos']
+        self.graph.nodes[node1_id]['pos'], self.graph.nodes[node2_id]['pos'] = node2_pos, node1_pos
+
+        self.__gdf_points.loc[self.__gdf_points['id'] == node1_id, 'geometry'] = Point(node2_pos)
+        self.__gdf_points.loc[self.__gdf_points['id'] == node2_id, 'geometry'] = Point(node1_pos)
+
+        for line_id, connected_point_id in self.__point_to_edges[node1_id]:
+            self.__update_line_geometry(line_id, node1_id, connected_point_id)
+        for line_id, connected_point_id in self.__point_to_edges[node2_id]:
+            self.__update_line_geometry(line_id, node2_id, connected_point_id)
+
+        return True
