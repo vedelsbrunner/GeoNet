a58d154b88ffe8f942a5d9079c24169bd2bac663
Valentin Edelsbrunner <valentin.edelsbrunner@automationx.com>
2024-01-10 16:27:25 +0100

    Introduces degree based sort in sunflower layout

--- scripts/layouts/SunflowerLayout.py
+++ scripts/layouts/SunflowerLayout.py
@@ -3,7 +3,6 @@ from scripts.layouts.Layout import Layout
 from scripts.layouts.SunflowerLayoutConfig import SunflowerLayoutConfig
 from scripts.utils.LoggerConfig import logger
 
-
 class SunflowerLayout(Layout):
     def __init__(self, clustering_strategy):
         super().__init__(clustering_strategy)
@@ -16,6 +15,9 @@ class SunflowerLayout(Layout):
 
         for cluster in clusters:
             cluster_points = points_gdf[points_gdf['cluster'] == cluster]
+
+            cluster_points = cluster_points.sort_values(by='degree', ascending=False)
+
             num_points = len(cluster_points)
             if num_points <= 1:
                 logger.debug("Skipping cluster with only one point")
@@ -30,5 +32,3 @@ class SunflowerLayout(Layout):
                 new_x = sunflower_center.x + r * math.cos(theta)
                 new_y = sunflower_center.y + r * math.sin(theta)
                 network.update_point(point.id, new_x, new_y)
-
-
